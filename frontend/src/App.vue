355b64300611fd5f7e8f152748635e7e30a9f77d
xknoth <xknoth@mendelu.cz>
2025-08-21 16:52:54 +0200

    Added components + they are added to views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <main class="test">
+  <Header />
+  <main class="main">
     <RouterView />
   </main>
+  <Footer />
 </template>
 
 <script setup lang="ts">
+  import Header from "@/components/Header.vue";
+  import Footer from "@/components/Footer.vue";
 </script>
 
 <style scoped>
--- frontend/src/router/pages/HomeView.vue
+++ frontend/src/router/pages/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
-  <p>ahoj</p>
+  <div class="home">
+    <CanbanBoard />
+  </div>
 </template>
 
 <script setup lang="ts">
-
+  import CanbanBoard from "@/components/CanbanBoard.vue";
 </script>
 
 <style scoped>
