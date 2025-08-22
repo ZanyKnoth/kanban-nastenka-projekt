5d96fd4b16a617b02af2313f774bffd7398726d3
xknoth <xknoth@mendelu.cz>
2025-08-22 20:13:00 +0200

    Added functions that will be needed in future for communication with backend

--- frontend/src/stores/kanbanStore.ts
+++ frontend/src/stores/kanbanStore.ts
@@ -7,5 +7,83 @@ import type { ErrorStatus } from "@/model/ErrorStatus";
 import { useRequest } from "@/composables/useRequest";
 
 export const useKanbanStore = defineStore('kanbanStore', () => {
+  const request = useRequest();
+  const error: Ref<string | null> = ref<string | null>(null);
 
+  async function getAllKanbanCards(): Promise<any> {
+    error.value = null;
+    const data: Ref<KanbanCard[]> = ref<KanbanCard[]>([]);
+
+    try {
+      data.value = await request.plainRequest(config.backendUrl + "/kanban/get-all-kanban-cards", { method: "GET" });
+
+    } catch (err: any) {
+      error.value = "Došlo k neočekávané chybě.";
+    }
+
+    return { err: error.value, data: data.value };
+  }
+
+  async function getKanbanCardById(id: string): Promise<{err: string, data: any}> {
+    error.value = null;
+    const data: Ref<KanbanCard | null> = ref<KanbanCard | null>(null);
+
+    try {
+      data.value = await request.plainRequeset(config.backendUrl + "/kanban/get-kanban-card/" + id, { method: "GET" });
+
+    } catch (err: any) {
+      if (err.response && err.response.status === 404) {
+        error.value = "Kanbanová kartička nebyla nalezena.";
+      } else {
+        error.value = "Došlo k neočekávané chybě.";
+      }
+    }
+
+    return { err: error.value, data: data.value };
+  }
+
+  async function createOrUpdateKanbanCard(data: KanbanCard): Promise<any> {
+    error.value = null;
+
+    try {
+      if(data.id == -1) {
+        await request.plainRequeset(config.backendUrl + "/kanban/create-kanban-card", { method: "POST", data: data });
+
+      } else {
+        await request.plainRequeset(config.backendUrl + "/kanban/update-kanban-card/" + data.id_age_category, { method: "PUT", data: data });
+      }
+    } catch (err: any) {
+      if (err.response) {
+        const errors: ErrorStatus = {
+          "404": "Kanbanová kartička nebyla nalezena.",
+          "400": "Chyba při vytváření kanbanové kartičky (název nemůže být prázdný, ...)."
+        }
+
+        error.value = errors[err.response.status.toString()];
+      } else {
+        error.value = "Došlo k neočekávané chybě.";
+      }
+    }
+
+    return { err: error.value };
+  }
+
+  async function deleteKanbanCard(id: string): Promise<any> {
+    error.value = null;
+
+    try {
+      await request.plainRequeset(config.backendUrl + "/kanban/delete-kanban-card/" + id, { method: "DELETE" });
+
+    } catch (err: any) {
+      if (err.response && err.response.status === 404) {
+        error.value = "Kanbanová kartička nebyla nalezena.";
+      } else {
+        error.value = "Došlo k neočekávané chybě.";
+      }
+    }
+
+    return { err: error.value };
+  }
+
+  return { getAllKanbanCards, getKanbanCardById, createOrUpdateKanbanCard, deleteKanbanCard }
 })
