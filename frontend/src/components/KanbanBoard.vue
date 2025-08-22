d6d68dc172d4e3c29d8a2045e86830c9ce98535f
xknoth <xknoth@mendelu.cz>
2025-08-22 15:15:57 +0200

    Added design and testing data to kanban board

--- frontend/src/components/KanbanBoard.vue
+++ frontend/src/components/KanbanBoard.vue
@@ -1,9 +1,18 @@
 <template>
-
+  <div class="kanban-board container-fluid py-3 d-flex gap-4">
+    <div class="kanban-board__col flex-fill">
+      <div class="kanban-board__card card shadow-sm h-100 rounded-3">
+        <div class="kanban-board__header p-3 d-flex align-items-center bg-light fw-bold rounded-3">
+          <div class="kanban-board__header-text mx-2">TODO <span>(6)</span></div>
+        </div>
+        <CanbanCard :title="'Content todo1 title'" :content="'Content todo1 text'"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+  import CanbanCard from "@/components/KanbanCard.vue";
 </script>
 
 <style scoped>
