48f9ab0a5e704116ab49f7b8eb265a6660f96fcf
xknoth <xknoth@mendelu.cz>
2025-08-22 20:11:27 +0200

    Added plain request function

--- frontend/src/composables/useRequest.ts
+++ frontend/src/composables/useRequest.ts
@@ -1,4 +1,15 @@
 import axios from 'axios';
 
 export function useRequest() {
+
+    const plainRequest = async(endpoint: string, options = {}): Promise<any> => {
+        const response = await axios({
+            url: `${endpoint}`,
+            ...options,
+        });
+
+        return response.data;
+    }
+
+    return { plainRequest };
 }
