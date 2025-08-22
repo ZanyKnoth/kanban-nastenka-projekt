b19d167134953302e4412fc9a5f8ff3716d03a3a
xknoth <xknoth@mendelu.cz>
2025-08-22 19:54:00 +0200

    Changed styles

--- frontend/src/components/KanbanCard.vue
+++ frontend/src/components/KanbanCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="kanban-board__body pt-0 py-2 px-1 bg-light d-flex flex-column gap-2 rounded-3">
+  <div class="kanban-board__body pt-0 py-2 px-1 d-flex flex-column gap-2 rounded-3">
     <div class="kanban-board__body-wrapper card p-2 shadow-sm">
       <div class="kanban-board__body-title fw-semibold h5">{{ props.title }}</div>
       <span class="kanban-board__body-content mt-1 h6">{{ props.content }}</span>
