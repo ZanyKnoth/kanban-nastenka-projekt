6bde296fa9bd0a13730e87d10380fb64394a9b3a
xknoth <xknoth@mendelu.cz>
2025-08-22 15:15:35 +0200

    Correct renaming of kanban vol. 2

---
+++ frontend/src/components/KanbanCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="kanban-board__body pt-0 py-2 px-1 bg-light d-flex flex-column gap-2 rounded-3">
+    <div class="kanban-board__body-wrapper card p-2 shadow-sm">
+      <div class="kanban-board__body-title fw-semibold h5">{{ props.title }}</div>
+      <span class="kanban-board__body-content mt-1 h6">{{ props.content }}</span>
+      <div class="kanban-board__link-wrapper d-flex justify-content-end">
+        <div class="badge bg-dark p-2">></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    title: string;
+    content: string;
+  }>()
+</script>
+
+<style scoped>
+
+</style>
