ac1307b8a344c1192ad6ae0203663453584ab569
xknoth <xknoth@mendelu.cz>
2025-08-22 14:50:01 +0200

    Changed design for main layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <Header />
-  <main class="main">
-    <RouterView />
-  </main>
-  <Footer />
+  <div class="app-container d-flex flex-column min-vh-100">
+    <main class="app-container__main d-flex flex-grow-1 ">
+      <div class="app-container__wrapper d-flex flex-grow-1 m-2">
+        <RouterView />
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script setup lang="ts">
-  import Header from "@/components/Header.vue";
   import Footer from "@/components/Footer.vue";
 </script>
 
 <style scoped>
-
+  .app-container {
+    background: rgba(0, 0, 255, .05);
+  }
+  .app-container__wrapper {
+    background: white;
+  }
 </style>
