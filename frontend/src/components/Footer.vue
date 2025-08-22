2753e4e98afc293f0ef1bf4e85dc70ee55903230
xknoth <xknoth@mendelu.cz>
2025-08-22 14:50:22 +0200

    Changed design for Footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <footer class="footer d-flex justify-content-center align-items-center p-2">
+    <span id="foot" class="footer__text fs-5">©Jan Knoth</span>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +9,11 @@
 </script>
 
 <style scoped>
+  .footer {
+    background: rgb(0, 0, 58);
+  }
 
+  .footer__text {
+    color: rgba(255,255,255,.5);
+  }
 </style>
