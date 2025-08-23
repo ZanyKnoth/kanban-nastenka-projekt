572e9653cda292d75e704e55d8ecbf586ece822e
xknoth <xknoth@mendelu.cz>
2025-08-23 11:53:50 +0200

    Added loading component for waiting during async operations

--- frontend/src/components/KanbanBoard.vue
+++ frontend/src/components/KanbanBoard.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="kanban-board container-fluid py-3 d-flex gap-4" :class="{'h-100 justify-content-center align-items-center': isLoading }">
-    <KanbanRow :title="'TO DO'" :active="6" :kanban-cards="[]"/>
+    <Loading v-if="isLoading" />
+    <KanbanRow v-else :title="'TO DO'" :active="6" :kanban-cards="[]"/>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { onMounted, ref } from "vue";
+  import type { Ref } from "vue";
   import KanbanRow from "@/components/KanbanRow.vue";
+  import Loading from "@/components/Loading.vue";
+
+  const isLoading: Ref<boolean> = ref<boolean>(true);
+  const loadingError: Ref<string | null> = ref<string | null>(null);
+
+  onMounted(async () => {
+    isLoading.value = false;
+  });
 </script>
 
 <style scoped>
