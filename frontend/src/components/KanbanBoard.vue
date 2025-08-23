a6134b7175a9f68ad679c84f8361f7fd7e044fe7
xknoth <xknoth@mendelu.cz>
2025-08-23 11:52:52 +0200

    Better code separation

--- frontend/src/components/KanbanBoard.vue
+++ frontend/src/components/KanbanBoard.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="kanban-board container-fluid py-3 d-flex gap-4">
-    <KanbanRow :title="'TO DO'" :active="6">
-      <template #kanban-cards>
-        <KanbanCard :title="'Content todo1 title'" :content="'Content todo1 text'"/>
-      </template>
-    </KanbanRow>
+  <div class="kanban-board container-fluid py-3 d-flex gap-4" :class="{'h-100 justify-content-center align-items-center': isLoading }">
+    <KanbanRow :title="'TO DO'" :active="6" :kanban-cards="[]"/>
   </div>
 </template>
 
 <script setup lang="ts">
   import KanbanRow from "@/components/KanbanRow.vue";
-  import KanbanCard from "@/components/KanbanCard.vue";
 </script>
 
 <style scoped>
--- frontend/src/components/KanbanRow.vue
+++ frontend/src/components/KanbanRow.vue
@@ -2,23 +2,25 @@
   <div class="kanban-board__col flex-fill">
     <div class="kanban-board__card shadow-sm h-100 rounded-3">
       <div class="kanban-board__header p-3 d-flex align-items-center fw-bold rounded-3">
-        <div class="kanban-board__header-text mx-2">{{ props.title }}<span>({{ props.active }})</span></div>
+        <div class="kanban-board__header-text mx-2">{{ props.title }} <span>({{ props.active }})</span></div>
       </div>
-      <slot name="kanban-cards"></slot>
+      <KanbanCard :title="'Content todo1 title'" :content="'Content todo1 text'"/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import KanbanCard from "@/components/KanbanCard.vue";
+
   const props = defineProps<{
     title: string;
     active: number;
-  }>()
+    kanbanCards: KanbanCard[];
+  }>();
 </script>
 
 <style scoped>
   .kanban-board__card {
     background: rgb(243, 243, 243);
   }
-
 </style>
