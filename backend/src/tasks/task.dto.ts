3497c7bdebdf1a4eb37b97a3dbe60752be588fd0
xknoth <xknoth@mendelu.cz>
2025-08-25 00:44:06 +0200

    Better data checking

--- backend/src/tasks/task.dto.ts
+++ backend/src/tasks/task.dto.ts
@@ -1,10 +1,14 @@
-import { IsString, IsIn, IsOptional } from 'class-validator';
+import { IsString, IsIn, IsOptional, IsNotEmpty, Matches } from 'class-validator';
 
 export class TaskDto {
     @IsString()
+    @IsNotEmpty({ message: 'Title cannot be empty' })
+    @Matches(/\S/, { message: 'Title cannot be only whitespace' })
     title: string;
 
     @IsString()
+    @IsNotEmpty({ message: 'Content cannot be empty' })
+    @Matches(/\S/, { message: 'Content cannot be only whitespace' })
     content: string;
 
     @IsString()
--- backend/src/tasks/task.schema.ts
+++ backend/src/tasks/task.schema.ts
@@ -5,7 +5,7 @@ export type TaskDocument = Task & Document;
 
 @Schema({ collection: 'tasks', timestamps: true })
 export class Task {
-    @Prop({ required: true })
+    @Prop()
     title: string;
 
     @Prop()
