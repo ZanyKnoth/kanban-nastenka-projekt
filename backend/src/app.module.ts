4ff8c717adee7f07b52a65294fe7bc6c2feeeb8e
xknoth <xknoth@mendelu.cz>
2025-08-24 17:23:01 +0200

    Added starting code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from "./tasks/task.controller";
+import { AppService } from "./tasks/task.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
