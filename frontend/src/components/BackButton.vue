c9b9d167d96f8d6fa38f8c16b46034e596452d1f
xknoth <xknoth@mendelu.cz>
2025-08-24 14:00:33 +0200

    Added back button component

---
+++ frontend/src/components/BackButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="back-link">
+  <component :is="typeof(link) == 'string' ? 'RouterLink' : 'router-link'" :to="link" class="back-link__link link-underline link-underline-opacity-0">
+    <font-awesome-icon :icon="['fa', 'arrow-left']" class="back-link__icon "/>
+    <span class="back-link__link-text h6">Zpět</span>
+  </component>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import type { LinkObject } from "@/model/LinkObject";
+
+  defineProps<{
+    link: string | LinkObject;
+  }>()
+</script>
+
+<style scoped>
+
+</style>
