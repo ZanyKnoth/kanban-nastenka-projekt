4e6dc3b95bd51cb55d5d2f09f0767b8d00a2e844
xknoth <xknoth@mendelu.cz>
2025-08-25 00:46:49 +0200

    Added Banner component

---
+++ frontend/src/components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="bannerClass">
+    <p v-for="message in props.messages" class="my-3">{{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed } from "vue";
+
+  const props = defineProps<{
+    messages: string[];
+    type: string;
+  }>()
+
+  const bannerClass = computed(() => {
+    const base = 'd-flex flex-column align-items-center text-center fw-bold rounded py-2 px-4 my-4 w-50'
+    const typeClasses = {
+      error: 'bg-danger text-white',
+      info: 'bg-primary text-white',
+    }
+    return `${base} ${typeClasses[props.type] || ''}`
+  })
+</script>
